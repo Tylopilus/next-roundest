e540e81f592e8c9003cf35568fa862d1f2e40207
Helge Ahrens <helgeahrens@Helges-MacBook-Pro.local>
2022-01-02 14:03:41 +0100

    added migration script to insert pokemon into db

---
+++ scripts/fill-db.ts
@@ -0,0 +1,20 @@
+import { PokemonClient } from 'pokenode-ts';
+import { prisma } from '../src/backend/utils/prisma';
+
+const doBackfill = async () => {
+  const pokeApi = new PokemonClient();
+  const allPokemons = await pokeApi.listPokemons(0, 151);
+  const pokemonByName = allPokemons.results.map((p, index) => ({
+    id: index + 1,
+    name: (p as { name: string }).name,
+    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+  const creation = await prisma.pokemon.createMany({
+    data: pokemonByName,
+  });
+
+  console.log('creation?', creation);
+};
+doBackfill();
